0c8be5cd778e73f78628f36e082069f4d2d3bdc5
Odis Clemons <odis.clemons@gmail.com>
2022-04-09 23:15:17 -0400

    added click handler for save button simplified getting time block

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function init() {
+  var descriptions = JSON.parse(localStorage.getItem("descriptions")) ?? {};
+
   for (let i = 9; i < 18; i++) {
     let t = i < 13 ? i : i - 12;
+    let desc = descriptions[`${i}`] ?? "";
     let timeBlock = `
-            <div data-timeBlock="${i}" class="row time-block">
+            <div class="row time-block">
                 <div class="col-1 hour">
                     <p>${t}${i < 12 ? "AM" : "PM"}</p>
                 </div>
-                <div class="col-10 description past" style="border: 1px solid white">
-                    <textarea></textarea>
+                <div class="col-10 description past">
+                    <textarea id="tb-${i}">${desc}</textarea>
                 </div>
-                <div class="col-1 saveBtn">
+                <div onclick="handleSave(${i})" class="col-1 saveBtn" role="button" alt="save">
                     <i class="fas fa-save"></i>
                 </div>
             </div>
@@ -18,4 +21,8 @@ function init() {
   }
 }
 
+var handleSave = (i) => {
+  //console.log(e.target.getAttribute("data-tb"));
+  console.log(i);
+};
 init();
